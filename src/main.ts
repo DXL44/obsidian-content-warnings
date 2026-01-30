5fe6abe222e452621eff77d7b6616f211d564ff2
DXL44 <28364459+DXL44@users.noreply.github.com>
2026-01-30 10:59:44 -0500

    basic modal opening on note open

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,35 @@ import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
+export default class HelloWorldPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		console.log("test peanut")
+
+
+		// do something when someone opens a file
+		this.registerEvent(this.app.workspace.on('file-open', (file) => {
+
+			// Only display a warning modal if the file is tagged with a CW
+			if (file)
+			{
+				console.log("You just opened " + file.path + "!! good job")
+				new SampleModal(this.app).open();
+			}
+			else
+			{
+				new Notice("Could not display content warning: file was null.");
+			}
+
+  	 	 }));
+
+
+
+
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -27,14 +41,7 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-modal-complex',
@@ -89,7 +96,7 @@ class SampleModal extends Modal {
 
 	onOpen() {
 		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Content warning: This note is tagged with (tag). Are you sure?');
 	}
 
 	onClose() {
